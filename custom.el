888176592e4d45502ac2e4c0ab307e70f1f4f0ef
Thales Loreto <thales.loreto@ifood.com.br>
2021-03-18 20:24:53 -0300

    First commit

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-buffer-switch-check-intermediate-buffers t)
+ '(flycheck-display-errors-delay 0.01)
+ '(flycheck-display-errors-function (quote flycheck-popup-tip-show-popup))
+ '(flycheck-emacs-lisp-load-path (quote inherit))
+ '(flycheck-flake8-maximum-line-length nil)
+ '(flycheck-indication-mode (quote right-fringe))
+ '(flycheck-popup-tip-error-prefix "✕ ")
+ '(global-flycheck-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
